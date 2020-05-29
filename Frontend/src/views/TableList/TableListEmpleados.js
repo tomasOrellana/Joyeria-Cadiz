0d8c80cae0145b8da7d04353ce09ab7a9b44847c
Francoco97 <fpalmatrejo@gmail.com>
2020-05-29 19:14:28 -0400

    Update TableListEmpleados.js

--- Frontend/src/views/TableList/TableListEmpleados.js
+++ Frontend/src/views/TableList/TableListEmpleados.js
@@ -1,14 +1,18 @@
-import React from "react";
-// @material-ui/core components
-import { makeStyles } from "@material-ui/core/styles";
-// core components
+import React from 'react';
+import PropTypes from 'prop-types';
+import { makeStyles } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Tabs from '@material-ui/core/Tabs';
+import Tab from '@material-ui/core/Tab';
+import Typography from '@material-ui/core/Typography';
+import Box from '@material-ui/core/Box';
 import GridItem from "components/Grid/GridItem.js";
 import GridContainer from "components/Grid/GridContainer.js";
 import Table from "components/Table/Table.js";
 import Card from "components/Card/Card.js";
-import CardHeader from "components/Card/CardHeader.js";
 import CardBody from "components/Card/CardBody.js";
-
+import CustomInput from "components/CustomInput/CustomInput.js";
+import Button from "components/CustomButtons/Button.js";
 const styles = {
   cardCategoryWhite: {
     "&,& a,& a:hover,& a:focus": {
@@ -36,37 +40,129 @@ const styles = {
       fontWeight: "400",
       lineHeight: "1"
     }
+  },
+  picker: {
+    height: 50
+  },
+  formControl: {
+    marginHorizontal: 10,
+    minWidth: 160,
+  },
+  selectEmpty: {
+    marginTop: 20,
+  },
+  root: {
+    flexGrow: 1,
+    backgroundColor: "#FFFFFF",
+  },
+  buscador: {
+    marginHorizontal: "10px"
+  },
+  boton: {
+    marginLeft: "20px"
   }
 };
 
+function TabPanel(props) {
+  const { children, value, index, ...other } = props;
+  return (
+    <div
+      role="tabpanel"
+      hidden={value !== index}
+      id={`simple-tabpanel-${index}`}
+      aria-labelledby={`simple-tab-${index}`}
+      {...other}
+    >
+      {value === index && (
+        <Box p={3}>
+          <Typography>{children}</Typography>
+        </Box>
+      )}
+    </div>
+  );
+}
+
+TabPanel.propTypes = {
+  children: PropTypes.node,
+  index: PropTypes.any.isRequired,
+  value: PropTypes.any.isRequired,
+};
+
+function a11yProps(index) {
+  return {
+    id: `simple-tab-${index}`,
+    'aria-controls': `simple-tabpanel-${index}`,
+  };
+}
+
 const useStyles = makeStyles(styles);
 
-export default function TableList() {
+export default function SimpleTabs() {
   const classes = useStyles();
+  const [value, setValue] = React.useState(0);
+
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+  };
+
   return (
     <GridContainer>
       <GridItem xs={12} sm={12} md={12}>
         <Card>
-          <CardHeader color="primary">
-            <h4 className={classes.cardTitleWhite}>Tabla de empleados</h4>
-            <p className={classes.cardCategoryWhite}>
-              Y sus datos
-            </p>
-          </CardHeader>
           <CardBody>
-            <Table
-              tableHeaderColor="primary"
-              tableHead={["Nombre", "Rut", "Tienda","Edad", "Salario"]}
-              tableData={[
-                ["Franco Palma", "19783062-k", "Lo Castillo", "22", "$9999999999"],
-                ["Diego Inostroza", "6969696-9", "La calle", "10", "Trabaja gratis, es putita"],
+      <AppBar position="static" color="primary" className={classes.Barrita}>
+        <Tabs value={value} onChange={handleChange} aria-label="simple tabs example">
+          <Tab label="Lo Castillo" {...a11yProps(0)} />
+          <Tab label="Apumanque" {...a11yProps(1)} />
+          <Tab label="Vitacura" {...a11yProps(2)} />
+        </Tabs>
+      </AppBar>
 
-              ]}
-            />
-          </CardBody>
-        </Card>
-      </GridItem>
+      <GridContainer className={classes.buscador} direction="row" justify>
+        <GridItem xs={2} sm={2} md={2}><CustomInput labelText="Numero" id="numero" formControlProps={{ fullWidth: true }} /></GridItem>
+        <GridItem xs={2} sm={2} md={2}><CustomInput labelText="Tipo" id="tipo" formControlProps={{ fullWidth: true }} /></GridItem>
+        <GridItem xs={2} sm={2} md={2}><CustomInput labelText="Material" id="material" formControlProps={{ fullWidth: true }} /></GridItem>
+        <GridItem xs={2} sm={2} md={2}><CustomInput labelText="Contacto" id="contacto" formControlProps={{ fullWidth: true }} /></GridItem>
+        <GridItem xs={2} sm={2} md={2}><CustomInput labelText="Rol" id="rol" formControlProps={{ fullWidth: true }} /></GridItem>
+        <GridItem xs={2} sm={2} md={2}><CustomInput labelText="Telefono" id="telefono" formControlProps={{ fullWidth: true }} /></GridItem>
+        <GridItem xs={2} sm={2} md={2}><CustomInput labelText="Salario" id="salario" formControlProps={{ fullWidth: true }} /></GridItem>
 
-    </GridContainer>
+        <GridItem xs={2} sm={2} md={2}><Button className={classes.boton} color="primary">Actualizar</Button></GridItem>
+      </GridContainer>
+
+      <TabPanel value={value} index={0}>
+        <Table
+          tableHeaderColor="primary"
+          tableHead={["Nombre", "Rut", "Tienda","Edad", "Rol","Telefono", "Salario"]}
+          tableData={[
+            ["Franco Palma", "19783062-k", "Lo Castillo", "22","Jefe supremo","132", "$9999999999"],
+            ["Diego Inostroza", "6969696-9", "La calle", "10","Putita","666", "Trabaja gratis, es putita"],
+          ]}
+        />
+      </TabPanel>
+      <TabPanel value={value} index={1}>
+      <Table
+        tableHeaderColor="primary"
+        tableHead={["Nombre", "Rut", "Tienda","Edad", "Rol","Telefono", "Salario"]}
+        tableData={[
+          ["Franco Palma", "19783062-k", "Lo Castillo", "22","Jefe supremo","132", "$9999999999"],
+          ["Diego Inostroza", "6969696-9", "La calle", "10","Putita","666", "Trabaja gratis, es putita"],
+        ]}
+      />
+      </TabPanel>
+      <TabPanel value={value} index={2}>
+      <Table
+        tableHeaderColor="primary"
+        tableHead={["Nombre", "Rut", "Tienda","Edad", "Rol","Telefono", "Salario"]}
+        tableData={[
+          ["Franco Palma", "19783062-k", "Lo Castillo", "22","Jefe supremo","132", "$9999999999"],
+          ["Diego Inostroza", "6969696-9", "La calle", "10","Putita","666", "Trabaja gratis, es putita"],
+        ]}
+      />
+      </TabPanel>
+      </CardBody>
+      </Card>
+    </GridItem>
+  </GridContainer>
   );
 }
