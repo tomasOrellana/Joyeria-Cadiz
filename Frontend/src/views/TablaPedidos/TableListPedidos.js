c64fa22e317f5a8589009c72c548bf3fe720a9ad
Francoco97 <fpalmatrejo@gmail.com>
2020-05-29 17:27:42 -0400

    Update TableListPedidos.js

--- Frontend/src/views/TablaPedidos/TableListPedidos.js
+++ Frontend/src/views/TablaPedidos/TableListPedidos.js
@@ -1,11 +1,11 @@
-import React from "react";
-// @material-ui/core components
-import { makeStyles } from "@material-ui/core/styles";
-import FormControl from '@material-ui/core/FormControl';
-import InputLabel from '@material-ui/core/InputLabel';
-import Select from '@material-ui/core/Select';
-import NativeSelect from '@material-ui/core/NativeSelect';
-// core components
+import React from 'react';
+import PropTypes from 'prop-types';
+import { makeStyles } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Tabs from '@material-ui/core/Tabs';
+import Tab from '@material-ui/core/Tab';
+import Typography from '@material-ui/core/Typography';
+import Box from '@material-ui/core/Box';
 import GridItem from "components/Grid/GridItem.js";
 import GridContainer from "components/Grid/GridContainer.js";
 import Table from "components/Table/Table.js";
@@ -14,6 +14,7 @@ import CardHeader from "components/Card/CardHeader.js";
 import CardBody from "components/Card/CardBody.js";
 import { container } from "assets/jss/material-dashboard-react";
 import { Container } from "@material-ui/core";
+import MenuItem from '@material-ui/core/MenuItem';
 
 const styles = {
   cardCategoryWhite: {
@@ -52,72 +53,98 @@ const styles = {
   },
   selectEmpty: {
     marginTop: 20,
+  },
+  root: {
+    flexGrow: 1,
+    backgroundColor: "#FFFFFF",
   }
 };
 
-const useStyles = makeStyles(styles);
-
-
-
-export default function TableList() {
-  const classes = useStyles();
-
-  const handleChange = (event) => {
-    const name = event.target.name;
-    setState({
-      ...state,
-      [name]: event.target.value,
-    });
-  };
-
-  const [state, setState] = React.useState({
-    age: '',
-    name: 'hai',
-  });
+function TabPanel(props) {
+  const { children, value, index, ...other } = props;
 
   return (
-    <GridContainer>
-    <Container className={classes.picker}>
-      <FormControl className={classes.formControl}>
-        <InputLabel htmlFor="age-native-simple">Sucursal</InputLabel>
-        <Select
-          native
-          value={state.age}
-          onChange={handleChange}
-          inputProps={{
-            name: 'age',
-            id: 'age-native-simple',
-          }}
-        >
-          <option aria-label="None" value="" />
-          <option value={1}>LO CASTILLO</option>
-          <option value={2}>APUMANQUE</option>
-          <option value={3}>VITACURA</option>
-        </Select>
-      </FormControl>
-    </Container>
-      <GridItem xs={12} sm={12} md={12}>
-        <Card>
-          <CardHeader color="primary">
-            <h4 className={classes.cardTitleWhite}>Tabla de pedidos</h4>
-            <p className={classes.cardCategoryWhite}>
-              Y sus datos
-            </p>
-          </CardHeader>
-          <CardBody>
-            <Table
-              tableHeaderColor="primary"
-              tableHead={["N° de Pedido", "Tipo", "Material","Contacto cliente", "Salario"]}
-              tableData={[
-                ["Franco Palma", "19783062-k", "Lo Castillo", "22", "$9999999999"],
-                ["Diego Inostroza", "6969696-9", "La calle", "10", "Trabaja gratis, es putita"],
+    <div
+      role="tabpanel"
+      hidden={value !== index}
+      id={`simple-tabpanel-${index}`}
+      aria-labelledby={`simple-tab-${index}`}
+      {...other}
+    >
+      {value === index && (
+        <Box p={3}>
+          <Typography>{children}</Typography>
+        </Box>
+      )}
+    </div>
+  );
+}
 
-              ]}
-            />
-          </CardBody>
-        </Card>
-      </GridItem>
+TabPanel.propTypes = {
+  children: PropTypes.node,
+  index: PropTypes.any.isRequired,
+  value: PropTypes.any.isRequired,
+};
 
-    </GridContainer>
+function a11yProps(index) {
+  return {
+    id: `simple-tab-${index}`,
+    'aria-controls': `simple-tabpanel-${index}`,
+  };
+}
+
+const useStyles = makeStyles(styles);
+
+export default function SimpleTabs() {
+  const classes = useStyles();
+  const [value, setValue] = React.useState(0);
+
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+  };
+
+  return (
+    <div className={classes.root}>
+      <AppBar position="static">
+        <Tabs value={value} onChange={handleChange} aria-label="simple tabs example">
+          <Tab label="Lo Castillo" {...a11yProps(0)} />
+          <Tab label="Apumanque" {...a11yProps(1)} />
+          <Tab label="Vitacura" {...a11yProps(2)} />
+        </Tabs>
+      </AppBar>
+      <TabPanel value={value} index={0}>
+      <Table
+        tableHeaderColor="primary"
+        tableHead={["N° de Pedido", "Tipo", "Material","Contacto cliente", "Salario"]}
+        tableData={[
+          [" Palma", "19783062-k", "Lo Castillo", "22", "$9999999999"],
+          [" Inostroza", "6969696-9", "La calle", "10", "Trabaja gratis, es putita"],
+
+        ]}
+      />
+      </TabPanel>
+      <TabPanel value={value} index={1}>
+      <Table
+        tableHeaderColor="primary"
+        tableHead={["N° de Pedido", "Tipo", "Material","Contacto cliente", "Salario"]}
+        tableData={[
+          ["Franco ", "19783062-k", "Lo Castillo", "22", "$9999999999"],
+          ["Diego ", "6969696-9", "La calle", "10", "Trabaja gratis, es putita"],
+
+        ]}
+      />
+      </TabPanel>
+      <TabPanel value={value} index={2}>
+      <Table
+        tableHeaderColor="primary"
+        tableHead={["N° de Pedido", "Tipo", "Material","Contacto cliente", "Salario"]}
+        tableData={[
+          ["Franco Palma", "19783062-k", "Lo Castillo", "22", "$9999999999"],
+          ["Diego Inostroza", "6969696-9", "La calle", "10", "Trabaja gratis, es putita"],
+
+        ]}
+      />
+      </TabPanel>
+    </div>
   );
 }
