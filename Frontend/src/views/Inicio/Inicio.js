cf01e69595ed6ed0a5418bc8c824300841551f64
fracasit0 <42551242+fracasit0@users.noreply.github.com>
2020-06-09 16:56:36 -0400

    aa

--- Frontend/src/views/Inicio/Inicio.js
+++ Frontend/src/views/Inicio/Inicio.js
@@ -41,247 +41,174 @@ import styles from "assets/jss/material-dashboard-react/views/dashboardStyle.js"
 
 const useStyles = makeStyles(styles);
 
-export default function Dashboard() {
-  const classes = useStyles();
-  return (
-    <div>
-      <GridContainer>
-
-        <GridItem xs={12} sm={6} md={3}>
-          <Card>
-            <CardHeader color="success" stats icon>
-              <CardIcon color="success">
-                <Icon>done</Icon>
-              </CardIcon>
-              <p className={classes.cardCategory}>Entregas</p>
-              <h3 className={classes.cardTitle}>
-                3 <small>listas</small>
-              </h3>
-            </CardHeader>
-            
-            <CardFooter stats>
-              <div className={classes.stats}>
-                <Update />
-                Mas info en seccion taller
-              </div>
-            </CardFooter>
-          </Card>
-        </GridItem>
-
-        <GridItem xs={12} sm={6} md={3}>
-          <Card>
-            <CardHeader color="warning" stats icon>
-              <CardIcon color="warning">
-                <Icon>palette</Icon>
-              </CardIcon>
-              <p className={classes.cardCategory}>Taller</p>
-              <h3 className={classes.cardTitle}>
-                4 <small>pedidos</small>
-              </h3>
-            </CardHeader>
-            <CardFooter stats>
-              <div className={classes.stats}>
-                <Update />
-                Mas info en seccion Taller
-              </div>
-            </CardFooter>
-          </Card>
-        </GridItem>
-        
-        <GridItem xs={12} sm={6} md={3}>
-          <Card>
-            <CardHeader color="info" stats icon>
-              <CardIcon color="info">
-                <Icon>shopping_basket</Icon>
-              </CardIcon>
-              <p className={classes.cardCategory}>Ventas diarias</p>
-              <h3 className={classes.cardTitle}>32</h3>
-            </CardHeader>
-            <CardFooter stats>
-              <div className={classes.stats}>
-                <LocalOffer />
-                Mas info en seccion ventas
-              </div>
-            </CardFooter>
-          </Card>
-        </GridItem>
-
-        <GridItem xs={12} sm={6} md={3}>
-          <Card>
-            <CardHeader color="danger" stats icon>
-              <CardIcon color="danger">
-                <Icon>person_pin</Icon>
-              </CardIcon>
-              <p className={classes.cardCategory}>Empleados</p>
-              <h3 className={classes.cardTitle}>
-                103 <small>totales</small>
-              </h3>
-            </CardHeader>
-            <CardFooter stats>
-              <div className={classes.stats}>
-                <LocalOffer />
-                Mas info en seccion empleados
-              </div>
-            </CardFooter>
-          </Card>
-        </GridItem>
-
-
-      </GridContainer>
-
-
-      <GridContainer>
-
-        <GridItem xs={12} sm={12} md={4}>
-          <Card chart>
-            <CardHeader color="success">
-              <ChartistGraph
-                className="ct-chart"
-                data={dailySalesChart.data}
-                type="Line"
-                options={dailySalesChart.options}
-                listener={dailySalesChart.animation}
-              />
-            </CardHeader>
-            <CardBody>
-              <h4 className={classes.cardTitle}>Ventas semanales</h4>
-              <p className={classes.cardCategory}>
-                <span className={classes.successText}>
-                  <ArrowUpward className={classes.upArrowCardCategory} /> 55%
-                </span>{" "}
-                Incremento de hoy.
-              </p>
-            </CardBody>
-            <CardFooter chart>
-              <div className={classes.stats}>
-                <AccessTime /> Actualizado hace 17 minutos
-              </div>
-            </CardFooter>
-          </Card>
-        </GridItem>
-
-        <GridItem xs={12} sm={12} md={4}>
-          <Card chart>
-            <CardHeader color="warning">
-              <ChartistGraph
-                className="ct-chart"
-                data={emailsSubscriptionChart.data}
-                type="Bar"
-                options={emailsSubscriptionChart.options}
-                responsiveOptions={emailsSubscriptionChart.responsiveOptions}
-                listener={emailsSubscriptionChart.animation}
-              />
-            </CardHeader>
-            <CardBody>
-              <h4 className={classes.cardTitle}>Ventas mensuales</h4>
-              <p className={classes.cardCategory}>asd</p>
-            </CardBody>
-            <CardFooter chart>
-              <div className={classes.stats}>
-                <AccessTime /> Actualizado hace 2 dias
-              </div>
-            </CardFooter>
-          </Card>
-        </GridItem>
-
-        <GridItem xs={12} sm={12} md={4}>
-          <Card chart>
-            <CardHeader color="danger">
-              <ChartistGraph
-                className="ct-chart"
-                data={completedTasksChart.data}
-                type="Line"
-                options={completedTasksChart.options}
-                listener={completedTasksChart.animation}
-              />
-            </CardHeader>
-            <CardBody>
-              <h4 className={classes.cardTitle}>Ventas diarias</h4>
-              <p className={classes.cardCategory}>asd</p>
-            </CardBody>
-            <CardFooter chart>
-              <div className={classes.stats}>
-                <AccessTime />Actualizado hace 5 minutos
-              </div>
-            </CardFooter>
-          </Card>
-        </GridItem>
-
-      </GridContainer>
-
-
-      {/*<GridContainer>
-      
-        <GridItem xs={12} sm={12} md={6}>
-          <CustomTabs
-            title="Tasks:"
-            headerColor="primary"
-            tabs={[
-              {
-                tabName: "Bugs",
-                tabIcon: BugReport,
-                tabContent: (
-                  <Tasks
-                    checkedIndexes={[0, 3]}
-                    tasksIndexes={[0, 1, 2, 3]}
-                    tasks={bugs}
-                  />
-                )
-              },
-              {
-                tabName: "Website",
-                tabIcon: Code,
-                tabContent: (
-                  <Tasks
-                    checkedIndexes={[0]}
-                    tasksIndexes={[0, 1]}
-                    tasks={website}
-                  />
-                )
-              },
-              {
-                tabName: "Server",
-                tabIcon: Cloud,
-                tabContent: (
-                  <Tasks
-                    checkedIndexes={[1]}
-                    tasksIndexes={[0, 1, 2]}
-                    tasks={server}
-                  />
-                )
-              }
-            ]}
-          />
-        </GridItem>
-
-        <GridItem xs={12} sm={12} md={6}>
-          <Card>
-            <CardHeader color="warning">
-              <h4 className={classes.cardTitleWhite}>Employees Stats</h4>
-              <p className={classes.cardCategoryWhite}>
-                New employees on 15th September, 2016
-              </p>
-            </CardHeader>
-            <CardBody>
-              <Table
-                tableHeaderColor="warning"
-                tableHead={["ID", "Name", "Salary", "Country"]}
-                tableData={[
-                  ["1", "Dakota Rice", "$36,738", "Niger"],
-                  ["2", "Minerva Hooper", "$23,789", "Curaçao"],
-                  ["3", "Sage Rodriguez", "$56,142", "Netherlands"],
-                  ["4", "Philip Chaney", "$38,735", "Korea, South"]
-                ]}
-              />
-            </CardBody>
-          </Card>
-        </GridItem>
-
-
-        
-      </GridContainer>
-              */}
-
-    </div>
-  );
+export default class Inicio extends React.Component {
+  render() {
+    return (
+      <div>
+        <GridContainer>
+  
+          <GridItem xs={12} sm={6} md={3}>
+            <Card>
+              <CardHeader color="success" stats icon>
+                <CardIcon color="success">
+                  <Icon>done</Icon>
+                </CardIcon>
+                <p style={styles.cardCategory}>Entregas</p>
+                <h3 style={styles.cardTitle}>
+                  3 <small>listas</small>
+                </h3>
+              </CardHeader>
+              
+              <CardFooter stats>
+                <div style={styles.stats}>
+                  <Update />
+                  Mas info en seccion taller
+                </div>
+              </CardFooter>
+            </Card>
+          </GridItem>
+  
+          <GridItem xs={12} sm={6} md={3}>
+            <Card>
+              <CardHeader color="warning" stats icon>
+                <CardIcon color="warning">
+                  <Icon>palette</Icon>
+                </CardIcon>
+                <p style={styles.cardCategory}>Taller</p>
+                <h3 style={styles.cardTitle}>
+                  4 <small>pedidos</small>
+                </h3>
+              </CardHeader>
+              <CardFooter stats>
+                <div style={styles.stats}>
+                  <Update />
+                  Mas info en seccion Taller
+                </div>
+              </CardFooter>
+            </Card>
+          </GridItem>
+          
+          <GridItem xs={12} sm={6} md={3}>
+            <Card>
+              <CardHeader color="info" stats icon>
+                <CardIcon color="info">
+                  <Icon>shopping_basket</Icon>
+                </CardIcon>
+                <p style={styles.cardCategory}>Ventas diarias</p>
+                <h3 style={styles.cardTitle}>32</h3>
+              </CardHeader>
+              <CardFooter stats>
+                <div style={styles.stats}>
+                  <LocalOffer />
+                  Mas info en seccion ventas
+                </div>
+              </CardFooter>
+            </Card>
+          </GridItem>
+  
+          <GridItem xs={12} sm={6} md={3}>
+            <Card>
+              <CardHeader color="danger" stats icon>
+                <CardIcon color="danger">
+                  <Icon>person_pin</Icon>
+                </CardIcon>
+                <p style={styles.cardCategory}>Empleados</p>
+                <h3 style={styles.cardTitle}>
+                  103 <small>totales</small>
+                </h3>
+              </CardHeader>
+              <CardFooter stats>
+                <div style={styles.stats}>
+                  <LocalOffer />
+                  Mas info en seccion empleados
+                </div>
+              </CardFooter>
+            </Card>
+          </GridItem>
+  
+  
+        </GridContainer>
+  
+  
+        <GridContainer>
+  
+          <GridItem xs={12} sm={12} md={4}>
+            <Card chart>
+              <CardHeader color="success">
+                <ChartistGraph
+                  style="ct-chart"
+                  data={dailySalesChart.data}
+                  type="Line"
+                  options={dailySalesChart.options}
+                  listener={dailySalesChart.animation}
+                />
+              </CardHeader>
+              <CardBody>
+                <h4 style={styles.cardTitle}>Ventas semanales</h4>
+                <p style={styles.cardCategory}>
+                  <span style={styles.successText}>
+                    <ArrowUpward style={styles.upArrowCardCategory} /> 55%
+                  </span>{" "}
+                  Incremento de hoy.
+                </p>
+              </CardBody>
+              <CardFooter chart>
+                <div style={styles.stats}>
+                  <AccessTime /> Actualizado hace 17 minutos
+                </div>
+              </CardFooter>
+            </Card>
+          </GridItem>
+  
+          <GridItem xs={12} sm={12} md={4}>
+            <Card chart>
+              <CardHeader color="warning">
+                <ChartistGraph
+                  style="ct-chart"
+                  data={emailsSubscriptionChart.data}
+                  type="Bar"
+                  options={emailsSubscriptionChart.options}
+                  responsiveOptions={emailsSubscriptionChart.responsiveOptions}
+                  listener={emailsSubscriptionChart.animation}
+                />
+              </CardHeader>
+              <CardBody>
+                <h4 style={styles.cardTitle}>Ventas mensuales</h4>
+                <p style={styles.cardCategory}>asd</p>
+              </CardBody>
+              <CardFooter chart>
+                <div style={styles.stats}>
+                  <AccessTime /> Actualizado hace 2 dias
+                </div>
+              </CardFooter>
+            </Card>
+          </GridItem>
+  
+          <GridItem xs={12} sm={12} md={4}>
+            <Card chart>
+              <CardHeader color="danger">
+                <ChartistGraph
+                  style="ct-chart"
+                  data={completedTasksChart.data}
+                  type="Line"
+                  options={completedTasksChart.options}
+                  listener={completedTasksChart.animation}
+                />
+              </CardHeader>
+              <CardBody>
+                <h4 style={styles.cardTitle}>Ventas diarias</h4>
+                <p style={styles.cardCategory}>asd</p>
+              </CardBody>
+              <CardFooter chart>
+                <div style={styles.stats}>
+                  <AccessTime />Actualizado hace 5 minutos
+                </div>
+              </CardFooter>
+            </Card>
+          </GridItem>
+  
+        </GridContainer>
+      </div>
+    );
+  }
 }
