c061bb5fe795c30fa9ffbcea3240401af6e009a1
dEmanu1001 <64873829+dEmanu1001@users.noreply.github.com>
2020-06-23 19:52:20 -0400

    Update routes.js
    ww

--- backend/SRC/app/routes/routes.js
+++ backend/SRC/app/routes/routes.js
@@ -372,14 +372,15 @@ router.get('/ventasdia', async function(req,res) {
 
 router.get('/ventasdia', async function(req,res) {
 		const fecha1 = new Date ();
-		const fecha2 = fecha1;
-		const fi = fecha1.concat("T00:00:00-04:00");
-		const ff = fecha2.concat("T23:59:00-04:00");
-		await venta.find({$and: [{fecha: {$gte: new Date(fecha1)}},{fecha: {$lt: new Date(fecha2)}}]}, (err, venta) => {
+		const fi = fecha1.substring(0,10).concet("T00:00:00+04:00");
+		const ff = fecha1.substring(0,10).concet("T23:59:00+04:00");
+		await venta.find({$and: [{fecha: {$gte: new Date(fi)}},{fecha: {$lt: new Date(ff)}}]}, (err, venta) => {
 			if(err) {
 				res.sendStatus(404);
 			}
 			else{
+				console.log(fi);
+				console.log(ff);
 				res.json(venta);
 			}
 		});
